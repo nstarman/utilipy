3a2a2168fc36e1a937e0998bdccfe547677bc5a8
Nathaniel Starkman <nstarkman@protonmail.com>
2019-05-09 15:04:37 -0400

    adding in folders and sample files

---
+++ scratch/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ----------------------------------------------------------------------------
+#
+# TITLE   : scratch initialization file
+# AUTHOR  : Nathaniel Starkman
+# PROJECT :
+#
+# ----------------------------------------------------------------------------
+
+### Docstring and Metadata
+r"""initialization file for scratch folder
+"""
+
+__author__ = "Nathaniel Starkman"
+__status__ = "Development"
+
+##############################################################################
+### Imports
+
+# General
+
+
+# Project-Specific
+
+
+##############################################################################
