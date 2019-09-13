63c52234ab0130aac56f768f8ce7515bcb657f0f
Nathaniel Starkman <nstarkman@protonmail.com>
2019-09-13 18:28:44 +0200

    started examples/folder

---
+++ astroPHD/__init__.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ----------------------------------------------------------------------------
+#
+# TITLE   : main initialization file
+#
+# ----------------------------------------------------------------------------
+
+### Docstring and Metadata
+"""main initialization file
+"""
+
+##############################################################################
+### IMPORTS
+
+## Custom
+from .util.logging import LogFile
+
+## Project-Specific
+# import top level packages
+from . import (
+    data_utils,
+    # fitting,
+    imports,
+    ipython,
+    math,
+    plot,
+    util
+)
+
+# import commonly used functions
+
+
+#############################################################################
+### Info
+
+__author__ = "Nathaniel Starkman, Shabaan Mohamed"
+__copyright__ = "Copyright 2019, "
+__credits__ = [""]
+__license__ = "GPL3"
+__version__ = "0.0.0"
+__maintainer__ = "Nathaniel Starkman"
+__email__ = "n.starkman@mail.utoronto.ca"
+__status__ = "Production"
+
+
+#############################################################################
+### END
---
+++ astroPHD/util/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ----------------------------------------------------------------------------
+#
+# TITLE   : util initialization file
+#
+# ----------------------------------------------------------------------------
+
+### Docstring and Metadata
+"""util initialization file
+"""
+
+##############################################################################
+### IMPORTS
+
+## Custom
+from .logging import LogPrint, LogFile  #, LoggerFile
+from .collections import ObjDict
+
+## Project-Specific
+
+#############################################################################
+### END
