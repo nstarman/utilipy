2b25c726744f30dc6fef84166cff097b9c8d5d68
Nathaniel Starkman <nstarkman@protonmail.com>
2019-09-13 18:24:39 +0200

    math functions
    starting with a basic quadrature function

---
+++ astroPHD/math/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ----------------------------------------------------------------------------
+#
+# TITLE   : math initialization file
+# AUTHOR  : Nathaniel Starkman
+#
+# ----------------------------------------------------------------------------
+
+### Docstring and Metadata
+"""initialization file for math
+"""
+
+__author__ = "Nathaniel Starkman"
+
+##############################################################################
+### IMPORTS
+
+## Project-Specific
+from .util import quadrature
+
+##############################################################################
+### DONE
---
+++ astroPHD/math/util.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ----------------------------------------------------------------------------
+#
+# TITLE   :
+# AUTHOR  : Nathaniel Starkman
+# PROJECT :
+#
+# ----------------------------------------------------------------------------
+
+### Docstring and Metadata
+"""**DOCSTRING**
+"""
+
+__author__ = "Nathaniel Starkman"
+# __copyright__ = "Copyright 2018, "
+# __credits__ = [""]
+# __license__ = "GPL3"
+# __version__ = "0.0.0"
+# __maintainer__ = "Nathaniel Starkman"
+# __email__ = "n.starkman@mail.utoronto.ca"
+# __status__ = "Production"
+
+
+##############################################################################
+### IMPORTS
+
+## General
+import numpy as np
+
+## Project-Specific
+
+
+##############################################################################
+### CODE
+
+def quadrature(*args, axis=0):
+    r"""return arguments summed in quadrature
+    """
+    return np.sqrt(np.sum(np.square(args), axis=axis))
+
+
+# --------------------------------------------------------------------------
+
+##############################################################################
+### END
