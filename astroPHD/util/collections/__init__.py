2951a2c753f36fdc472dcf25d40cc1021247d157
Nathaniel Starkman <nstarkman@protonmail.com>
2019-09-13 18:25:13 +0200

    ObDict in Collections

---
+++ astroPHD/util/collections/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ----------------------------------------------------------------------------
+#
+# TITLE   : ipython initialization file
+# AUTHOR  : Nathaniel Starkman
+#
+# ----------------------------------------------------------------------------
+
+### Docstring and Metadata
+"""initialization file for collections
+"""
+
+__author__ = "Nathaniel Starkman"
+
+
+##############################################################################
+### IMPORTS
+
+## Project-Specific
+from ._ObjDict import ObjDict
+
+##############################################################################
+### CODE
