4a6462b9cb59fa4003937ac87e701c110e19a14d
Nathaniel Starkman <nstarkman@protonmail.com>
2019-09-18 16:39:54 -0400

    added getfullerargspec in util.inspect
    an improvement on getfullargspec in inspect. This separates the arguments with and without default values.

---
+++ astroPHD/util/inspect/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ----------------------------------------------------------------------------
+#
+# TITLE   : inspect initialization file
+#
+# ----------------------------------------------------------------------------
+
+### Docstring and Metadata
+"""initialization file for `inspect`
+"""
+
+__author__ = "Nathaniel Starkman"
+
+##############################################################################
+### IMPORTS
+
+## General
+from inspect import *
+
+## Project-Specific
+from ._getfullerargspec import FullerArgSpec, getfullerargspec
+
+
+##############################################################################
+### END
---
+++ astroPHD/util/inspect/_getfullerargspec.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ----------------------------------------------------------------------------
+#
+# TITLE   : inspect
+# AUTHOR  : Nathaniel Starkman
+# PROJECT : astroPHD
+#
+# ----------------------------------------------------------------------------
+
+### Docstring and Metadata
+"""**DOCSTRING**
+"""
+
+__author__ = "Nathaniel Starkman"
+
+
+##############################################################################
+### IMPORTS
+
+## General
+from inspect import getfullargspec
+from collections import namedtuple
+
+
+##############################################################################
+### Types
+
+FullerArgSpec = namedtuple('FullerArgSpec',
+                           ['args', 'defaultargs', 'argdefaults', 'varargs',
+                            'kwonlyargs', 'kwonlydefaults', 'varkw',
+                            'annotations'])
+
+
+##############################################################################
+### Functions
+
+def getfullerargspec(func):
+    """getfullerargspec
+
+    fullargspec with separation of mandatory and optional arguments
+    adds *defargs* with corresponding *defaults*
+
+    Parameters
+    ----------
+    func : function
+        the function to inspect
+
+    Returns
+    -------
+    FullerArgSpec : namedtuple
+        args             : the mandatory arguments
+        defargs          : arguments with defaults
+        defaults         : dictionary of defaults to defargs
+        varargs          : variable arguments (*args)
+        kwonlyargs       : key-word only arguments
+        kwonlydefaults   : key-word only argmunent defaults
+        varkw            : variable key-word arguments (**kwargs)
+        annotations      : function annotations
+    """
+    spec = getfullargspec(func)
+
+    if spec.defaults is not None:
+
+        args = spec.args[:-len(spec.defaults)]
+        defargs = spec.args[-len(spec.defaults):]
+        defaults = {k: v for k, v in zip(defargs, spec.defaults)}
+
+    else:
+        args = spec.args
+        defargs = None
+        defaults = None
+
+    return FullerArgSpec(args, defargs, defaults, spec.varargs,
+                         spec.kwonlyargs, spec.kwonlydefaults, spec.varkw,
+                         spec.annotations)
+# /def
+
+
+##############################################################################
+### END
