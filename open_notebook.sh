c95c2e76cf80b8a24bfbd9c724a3ab15b51268f7
nstarman <nstarman@users.noreply.github.com>
2019-09-17 21:57:13 -0400

    Update open_notebook.sh

--- open_notebook.sh
+++ open_notebook.sh
@@ -11,14 +11,19 @@
 ###############################################################################
 ### RUN
 
-# Read correct anaconda environment
+# Read correct Anaconda environment
 source envs/main_env.sh
 
-# Activate the correct anaconda environment
+# Activate the correct Anaconda environment
 source activate ./envs/$main_env
 
 # Open a jupyter notebook
 jupyter lab ./
+
+###############################################################################
+### ChangeLog
+# file written by James Lane
+# rewritten by Nathaniel Starkman for arbitary Anaconda env, not prespecified
 
 ###############################################################################
 ### END
