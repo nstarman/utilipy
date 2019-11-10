259e52fd23dbba700dd5a6790ea830d71344650f
Nathaniel Starkman <nstarkman@protonmail.com>
2019-11-09 19:13:05 -0500

    readthedocs hooks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../../src'))
 
 
 # -- Project information -----------------------------------------------------
@@ -23,6 +24,8 @@ author = 'Nathaniel Starkman'
 
 
 # -- General configuration ---------------------------------------------------
+
+master_doc = 'index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
