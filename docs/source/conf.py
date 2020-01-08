24dd4b24c21dbb1bccdb905fd92e33d2f1c896e1
Nathaniel Starkman <nstarkman@protonmail.com>
2020-01-08 15:07:41 -0500

    revert docs theme
    going to switch to astropy
    also learned lesson. Will be doing this stuff in branches.
    
    Signed-off-by: Nathaniel Starkman <nstarkman@protonmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,31 +4,16 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-
-"""astroPHD sphinx configuration file.
-
-modelled off of the matplotlib conf.py
-
-"""
-
 # -- Path setup --------------------------------------------------------------
-
-import os
-import sys
-
-# import astroPHD  # TODO need to make pip-able first
-import sphinx
-
-from datetime import datetime
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
+import os
+import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../../src'))
-sys.path.insert(0, os.path.abspath('../../templates'))
-# sys.path.append(os.path.abspath('sphinxext'))
 
 
 # -- Project information -----------------------------------------------------
@@ -36,12 +21,6 @@ sys.path.insert(0, os.path.abspath('../../templates'))
 project = 'astroPHD'
 copyright = '2019, Nathaniel Starkman'
 author = 'Nathaniel Starkman'
-
-# version = astroPHD.__version__  # TODO need to make pip-able first
-version = '0.1.1'
-release = version
-
-github_project_url = "https://github.com/nstarman/astroPHD/"
 
 
 # -- General configuration ---------------------------------------------------
@@ -52,11 +31,8 @@ master_doc = 'index'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    # sphinx
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
     'sphinx.ext.doctest',
-    'sphinx.ext.inheritance_diagram',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
@@ -66,16 +42,6 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinx.ext.napoleon',
-    # docstrings
-    'numpydoc',
-    # sphinx_gallery
-    # 'sphinx_gallery.gen_gallery',
-    # ipython
-    'IPython.sphinxext.ipython_directive',
-    'IPython.sphinxext.ipython_console_highlighting',
-    # matplotlib
-    'matplotlib.sphinxext.mathmpl',
-    'matplotlib.sphinxext.plot_directive',
     # for markdown
     'recommonmark',
     'sphinx_markdown_tables',
@@ -100,37 +66,21 @@ source_suffix = {
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['Thumbs.db', '.DS_Store',
-                    # community codes
-                    '../../src/src/astroPHD/community/starkplot',
-                    '../src/src/astroPHD/community/starkplot']
+                    # comunity codes
+                    '../../src/src/astroPHD/community/starkplot']
 
 
 # -- Options for HTML output -------------------------------------------------
 
-# The style sheet to use for HTML and HTML Help pages. A file of that name
-# must exist either in Sphinx' static/ path, or in one of the custom paths
-# given in html_static_path.
-html_style = 'default.css'
-
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinxdoc'
-
-# The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-# If false, no module index is generated.
-#html_use_modindex = True
-html_domain_indices = ["py-modindex"]
-
-# Output file base name for HTML help builder.
-htmlhelp_basename = 'astroPHDdoc'
 
 
 # -- Extension configuration -------------------------------------------------
@@ -146,58 +96,9 @@ numpydoc_xref_param_type = True  # Whether to create cross-references for the pa
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3', None),
-    'numpy': ('https://docs.scipy.org/doc/numpy/', None),
-    'scipy': ('https://docs.scipy.org/doc/scipy/reference/', None),
-    'pandas': ('https://pandas.pydata.org/pandas-docs/stable/', None),
-    'Pillow': ('https://pillow.readthedocs.io/en/stable/', None),
-    'cycler': ('https://matplotlib.org/cycler', None),
-    'dateutil': ('https://dateutil.readthedocs.io/en/stable/', None),
-}
+intersphinx_mapping = {'https://docs.python.org/3/': None}
 
 # -- Options for todo extension ----------------------------------------------
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
-
-
-# -- Options for LaTeX output ---------------------------------------
-
-# The paper size ('letter' or 'a4').
-latex_paper_size = 'a4'
-
-
-
-# Show both class-level docstring and __init__ docstring in class
-# documentation
-autoclass_content = 'both'
-
-
-# -- Options for numpydoc extension ---------------------------------------
-
-numpydoc_show_class_members = False
-
-latex_engine = 'xelatex'  # or 'lualatex'
-
-latex_elements = {
-    'babel': r'\usepackage{babel}',
-    'fontpkg': r'\setmainfont{DejaVu Serif}',
-}
-
-html4_writer = True
-
-inheritance_node_attrs = dict(fontsize=16)
-
-
-def setup(app):
-    if any(st in version for st in ('post', 'alpha', 'beta')):
-        bld_type = 'dev'
-    else:
-        bld_type = 'rel'
-    app.add_config_value('releaselevel', bld_type, 'env')
-
-
-
-##############################################################################
-# END
