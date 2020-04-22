ade79fe20c5ce0b150a57cdffeb04ae3c1d85e70
Nathaniel Starkman <nstarkman@protonmail.com>
2020-04-21 23:50:35 -0400

    ipython tests
    Signed-off-by: Nathaniel Starkman <nstarkman@protonmail.com>

--- utilipy/ipython/tests/__init__.py
+++ utilipy/ipython/tests/__init__.py
@@ -4,12 +4,27 @@
 """Tests for :mod:`~utilipy.ipython`."""
 
 
+__all__ = [
+    "test_autoreload",
+    "test_imports",
+    "test_notebook",
+    "test_plot",
+    "test_printing",
+]
 
 
 ##############################################################################
 # IMPORTS
 
 # PROJECT-SPECIFIC
+
+from . import (
+    test_autoreload,
+    test_imports,
+    test_notebook,
+    test_plot,
+    test_printing,
+)
 
 
 ##############################################################################
