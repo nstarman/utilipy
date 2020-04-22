bfe3e2c9833e7b97ecf0d6623a3f135fcbe7ae9d
Nathaniel Starkman <nstarkman@protonmail.com>
2020-04-21 23:52:54 -0400

    scripts tests structure
    nothing to test currently
    
    Signed-off-by: Nathaniel Starkman <nstarkman@protonmail.com>

--- utilipy/scripts/tests/__init__.py
+++ utilipy/scripts/tests/__init__.py
@@ -1,29 +1,7 @@
 # -*- coding: utf-8 -*-
 # see LICENSE.rst
 
-# ----------------------------------------------------------------------------
-#
-# TITLE   :
-# AUTHOR  :
-# PROJECT :
-#
-# ----------------------------------------------------------------------------
-
-"""Initialization file.
-
-Routine Listings
-----------------
-
-"""
-
-__author__ = ""
-# __copyright__ = "Copyright 2018, "
-# __credits__ = [""]
-# __license__ = ""
-# __version__ = "0.0.0"
-# __maintainer__ = ""
-# __email__ = ""
-# __status__ = "Production"
+"""Scripts Tests."""
 
 # __all__ = [
 #     ""
