cc97ea94ee60f2e5d003c0b1e3dfde18e2194db7
Nathaniel Starkman <nstarkman@protonmail.com>
2020-06-28 22:20:09 -0400

    Astropy typing
    Signed-off-by: Nathaniel Starkman <nstarkman@protonmail.com>

--- utilipy/utils/typing.py
+++ utilipy/utils/typing.py
@@ -9,9 +9,16 @@ __all__ = [
     # numpy
     "array_like",
     # Astropy
-    "TableType",
+    "CoordinateRepresentationType",
+    "CoordinateDifferentialType",
     "CoordinateType",
     "FrameOptionsType",
+    "TableType",
+    "UnitableType",
+    "DistanceType",
+    "AngleType",
+    "ModelType",
+    "FittableModelType",
 ]
 
 
@@ -27,7 +34,12 @@ from typing import *  # NOQA
 # THIRD PARTY
 
 import numpy as np
-from astropy import table, coordinates as coord, units as u
+
+import astropy.coordinates as coord
+import astropy.units as u
+
+from astropy import table
+from astropy.modeling.core import Model, FittableModel, CompoundModel
 
 
 ###############################################################################
@@ -57,6 +69,36 @@ Examples
 
 #####################################################################
 # Astropy
+
+CoordinateRepresentationType = T.TypeVar(
+    "RepresentationType",
+    coord.BaseRepresentation,
+    coord.BaseRepresentationOrDifferential,
+)
+"""Astropy [astropy]_ representation types.
+
+References
+----------
+.. [astropy] Astropy Collaboration et al., 2018, AJ, 156, 123.
+
+"""
+
+
+CoordinateDifferentialType = T.TypeVar(
+    "RepresentationType",
+    coord.BaseDifferential,
+    coord.BaseRepresentationOrDifferential,
+)
+"""Astropy [astropy]_ representation types.
+
+References
+----------
+.. [astropy] Astropy Collaboration et al., 2018, AJ, 156, 123.
+
+"""
+
+
+# -------------------------------------------------------------------
 
 CoordinateType = T.TypeVar(
     "CoordinateType", coord.BaseCoordinateFrame, coord.SkyCoord
@@ -96,6 +138,8 @@ Examples
 
 """
 
+# -------------------------------------------------------------------
+
 TableType = T.TypeVar("TableType", table.Table, table.QTable)
 """Astropy [astropy]_ Table-types.
 
@@ -114,8 +158,10 @@ Examples
 
 """
 
+# -------------------------------------------------------------------
+
 UnitableType = T.TypeVar("UnitableType", u.Unit, u.Quantity, str)
-"""Types that work with Astropy [astropy]_ Unit().
+"""Types that work with Astropy [astropy]_ :class:`~astropy.units.Unit`.
 
 Subclasses of Unit, Quantity.
 
@@ -128,6 +174,102 @@ Examples
 
     >>> x: UnitableType = 10 * u.km
     >>> isinstance(x, u.Quantity)
+    True
+
+"""
+
+QuantityType = T.TypeVar("QuantityType", u.Quantity, u.SpecificTypeQuantity)
+"""Types for :class:`~astropy.units.Quantity`[astropy]_ .
+
+Subclasses of Distance, Quantity.
+
+References
+----------
+.. [astropy] Astropy Collaboration et al., 2018, AJ, 156, 123.
+
+Examples
+--------
+
+    >>> x: QuantityType = 10 * u.km
+    >>> isinstance(x, u.Quantity)
+    True
+
+"""
+
+DistanceType = T.TypeVar("DistanceType", coord.Distance, u.Quantity)
+"""Types for :class:`~astropy.coordinates.Distance`[astropy]_ .
+
+Subclasses of Distance, Quantity.
+
+References
+----------
+.. [astropy] Astropy Collaboration et al., 2018, AJ, 156, 123.
+
+Examples
+--------
+
+    >>> x: DistanceType = 10 * u.km
+    >>> isinstance(x, u.Quantity)
+    True
+
+"""
+
+
+AngleType = T.TypeVar("DistanceType", coord.Angle, u.Quantity)
+"""Types for :class:`~astropy.coordinates.Angle`[astropy]_ .
+
+Subclasses of Angle, Quantity.
+
+References
+----------
+.. [astropy] Astropy Collaboration et al., 2018, AJ, 156, 123.
+
+Examples
+--------
+
+    >>> x: DistanceType = 10 * u.deg
+    >>> isinstance(x, u.Quantity)
+    True
+
+"""
+
+# -------------------------------------------------------------------
+
+ModelType = T.TypeVar("ModelType", Model, CompoundModel)
+"""Astropy :class:`~astropy.modeling.Model` Types [astropy]_.
+
+Subclasses of Model, CompoundModel.
+
+References
+----------
+.. [astropy] Astropy Collaboration et al., 2018, AJ, 156, 123.
+
+Examples
+--------
+
+    >>> from astropy.modeling.models import Model, Gaussian1D
+    >>> x: ModelType = Gaussian1D()
+    >>> isinstance(x, Model)
+    True
+
+"""
+
+FittableModelType = T.TypeVar("ModelType", FittableModel, CompoundModel)
+"""Astropy :class:`~astropy.modeling.FittableModel` Types [astropy]_.
+
+Subclasses of FittableModel, CompoundModel.
+Note that not all CompoundModels are fittable, but this can't be checked.
+
+References
+----------
+.. [astropy] Astropy Collaboration et al., 2018, AJ, 156, 123.
+
+Examples
+--------
+
+    >>> from astropy.modeling.models import Model, Gaussian1D
+    >>> x: ModelType = Gaussian1D()
+    >>> isinstance(x, Model)
     True
 
 """
