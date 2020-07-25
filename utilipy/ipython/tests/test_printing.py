d930bc1dd9114f4d88bbad6622de6c6ce5d78b17
Nathaniel Starkman <nstarkman@protonmail.com>
2020-07-24 23:09:21 -0400

    test printing
    Signed-off-by: Nathaniel Starkman <nstarkman@protonmail.com>

--- utilipy/ipython/tests/test_printing.py
+++ utilipy/ipython/tests/test_printing.py
@@ -3,7 +3,7 @@
 """Tests for :mod:`~utilipy.ipython.printing`."""
 
 
-__all__ = ["test_printing"]
+__all__ = ["test_printMD"]
 
 
 ##############################################################################
@@ -30,16 +30,48 @@ from .. import printing
 ##############################################################################
 
 
-@pytest.mark.skip(reason="TODO")
-def test_printing():
-    """Test :class:`~utilipy.ipython.printing`."""
-    pass
+def test_printMD():  # TODO actual test
+    """Test :class:`~utilipy.ipython.printing.printMD`."""
+    # all major options
+    printing.printMD(
+        "test",
+        color="green",
+        size=32,
+        fontweight=15,
+        fontstyle="italic",
+        highlight=True,
+    )
+
+    # all minor options
+    printing.printMD(
+        "test", bold=True, italic=True,
+    )
 
 
 # /def
 
 
 # -------------------------------------------------------------------
+
+
+def test_printLTX():  # TODO actual test
+    """Test :class:`~utilipy.ipython.printing.printMD`."""
+    # major options
+    printing.printLTX("test", label="", math="$$")
+
+    # minor options
+    printing.printLTX("test", math=True)
+
+    printing.printLTX("i=0", equation=True, label="i_eq_0")
+
+    printing.printLTX("i=1", equation=None, label="i_eq_1")
+
+    printing.printLTX("i=2", matrix="b", label="i_eq_2")
+
+    printing.printLTX("i=2", matrix=True, label="i_eq_2")
+
+
+# /def
 
 
 ##############################################################################
