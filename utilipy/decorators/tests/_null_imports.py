5fc579281bcf6434c1cb226a15c918609fd13139
Nathaniel Starkman <nstarkman@protonmail.com>
2020-07-24 23:08:20 -0400

    test_docstring
    Signed-off-by: Nathaniel Starkman <nstarkman@protonmail.com>

---
+++ utilipy/decorators/tests/_null_imports.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Null set of imports for testing.
+
+Routine Listings
+----------------
+Nothing.
+
+Notes
+-----
+This is not a real set of imports
+
+"""
+
+##############################################################################
+# END
---
+++ utilipy/decorators/tests/test_docstring.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+"""Test contents of :mod:`~utilipy.decorators.docstring`."""
+
+__all__ = [
+    "test_set_docstring_for_import_func",
+]
+
+
+##############################################################################
+# IMPORTS
+
+# PROJECT-SPECIFIC
+
+from .. import docstring
+from . import _null_imports
+
+
+##############################################################################
+# PARAMETERS
+
+
+##############################################################################
+# CODE
+##############################################################################
+
+
+def test_set_docstring_for_import_func():
+    """Test :func:`~utilipy.decorators.docstring.set_docstring_for_import_func`."""
+
+    @docstring.set_docstring_for_import_func(
+        "tests/_null_imports.py",
+        package="utilipy.decorators",
+        section="Routine Listings",
+    )
+    def null_imports_func():
+        return
+
+    # /def
+
+    assert null_imports_func.__doc__ == "Nothing."
+
+    _null_imports
+
+
+# /def
+
+
+# -------------------------------------------------------------------
+
+
+##############################################################################
+# END
